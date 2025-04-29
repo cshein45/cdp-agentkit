1c6e190b45fd8d08220a9fb81a2e824329eb122d
Philippe d'Argent <p.dargent@cern.ch>
2025-04-29 12:23:42 +0200

    add zora action provider

--- typescript/examples/langchain-cdp-chatbot/chatbot.ts
+++ typescript/examples/langchain-cdp-chatbot/chatbot.ts
@@ -10,6 +10,7 @@ import {
   pythActionProvider,
   openseaActionProvider,
   alloraActionProvider,
+  // zoraActionProvider,
 } from "@coinbase/agentkit";
 import { getLangChainTools } from "@coinbase/agentkit-langchain";
 import { HumanMessage } from "@langchain/core/messages";
@@ -122,6 +123,10 @@ async function initializeAgent() {
             ]
           : []),
         alloraActionProvider(),
+        // zoraActionProvider({
+        //   privateKey: process.env.ZORA_PRIVATE_KEY || "",
+        //   RPC_URL: process.env.ZORA_RPC_URL,
+        // }),
       ],
     });
 
